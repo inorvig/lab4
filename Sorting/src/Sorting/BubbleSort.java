4880c3cc245ae071ddb1b4c7345874e8e55735e6
isabella_norvig@brown.edu <inorvig@cs.brown.edu>
2014-02-28 00:15:32 -0500

    bubble sort

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -7,8 +7,21 @@ public class BubbleSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
+		int i,temp;
+    	boolean cont = true;
+    	while (cont){
+    		cont = false;
+    		for (i=0;i<array.length-1;i++){
+    			if (array[i]>array[i+1]){
+        			temp = array[i];
+        			array[i]=array[i+1];
+        			array[i+1]=temp;
+        			cont = true;
+        		}
+    		}
+    		
+    	}
+
+        return unsortedArray;
 	}
 }
